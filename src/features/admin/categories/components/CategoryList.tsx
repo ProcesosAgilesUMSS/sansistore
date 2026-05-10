758aa47b6509474663cffafb28c02d34a23956c0
Jherlan Marcelo Calero Sardan <jherlan.calero@gmail.com>
2026-05-10 06:11:57 -0400

    fix(#151): category with modals,fix active status

--- src/features/admin/categories/components/CategoryList.tsx
+++ src/features/admin/categories/components/CategoryList.tsx
@@ -48,7 +48,7 @@ export default function CategoryList() {
     }
   }, []);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+ // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     if (hasFetched.current) return;
     hasFetched.current = true;
