55e7664ca88e82203e6a75a03ddaa0a851eed5de
jh0nz <jhondeycraft776@gmail.com>
2026-05-15 13:31:28 -0400

    feat: update PWA configuration and layout for improved manifest support

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,40 +22,41 @@ export default defineConfig({
         description: 'Tienda en línea',
         theme_color: '#88b04b',
         background_color: '#fffbf4',
-        display: 'fullscreen',
+        display: 'standalone',
         orientation: 'portrait',
         scope: '/',
         start_url: '/',
+        lang: 'es',
         icons: [
           {
             src: '/pwa-144x144.png',
             sizes: '144x144',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           },
           {
             src: '/pwa-384x384.png',
             sizes: '384x384',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           },
           {
             src: '/pwa-1024x1024.png',
             sizes: '1024x1024',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
         ],
       },
