980415ca5247660340c0bbf5adb85c95cf562489
Marca7i <josepinedogonza@gmail.com>
2026-05-21 16:17:37 -0400

    HU terminada

--- src/features/seller/constants/sections.ts
+++ src/features/seller/constants/sections.ts
@@ -1,7 +1,12 @@
 export const sections = [
+
+   {
+    id: 'reservados', label: 'Pedidos Reservados', route: '/seller/reserved-orders'
+  },
   {
     id: 'entregados', label: 'Pedidos Confirmados', route: '/seller/orders'
   },
+ 
   {
     id: 'asignar', label: 'Pedidos Listos para Asignar', route: '/seller/assign'
   }
