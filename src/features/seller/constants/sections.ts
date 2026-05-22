f370a1092df3cbd674779b09c2b36746af94189f
epf-21 <efrainperalta.f@gmail.com>
2026-05-22 09:29:46 -0400

    fix: cambiar el estado de CONFIRMADO a estado EMPAQUETADO siguiendo el nuevo flujo

--- src/features/seller/constants/sections.ts
+++ src/features/seller/constants/sections.ts
@@ -1,30 +1,32 @@
 export const sections = [
-
-   {
-    id: 'reservados', label: 'Pedidos Reservados', route: '/seller/reserved-orders'
+  {
+    id: 'reservados',
+    label: 'Pedidos Reservados',
+    route: '/seller/reserved-orders'
   },
   {
     id: 'entregados',
-    label: 'Pedidos Confirmados',
+    label: 'Pedidos Empaquetados',
     route: '/seller/orders',
   },
   {
-    id: 'historial',
-    label: 'Historial de pedidos',
-    route: '/seller/order-history',
-  },
- 
-  {
     id: 'asignar',
-    label: 'Pedidos Listos para Asignar',
+    label: 'Asignar a Mensajero',
     route: '/seller/assign',
   },
   {
-    id: 'compra', label: 'Registrar Compra', route: '/seller/purchase'
+    id: 'compra',
+    label: 'Registrar Compra',
+    route: '/seller/purchase'
   },
   {
     id: 'rendicion',
     label: 'Rendicion del dia',
     route: '/seller/daily-collections',
+  },
+  {
+    id: 'historial',
+    label: 'Historial de pedidos',
+    route: '/seller/order-history',
   },
 ];
