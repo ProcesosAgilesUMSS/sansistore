4536146641bd48ed43a20bdd46523abf91036cce
epf-21 <efrainperalta.f@gmail.com>
2026-05-29 15:46:06 -0400

    fix: change order allocation logic

--- src/features/seller/hooks/useAssignOrdersToDelivery.ts
+++ src/features/seller/hooks/useAssignOrdersToDelivery.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { assignCourierToDelivery } from "../services/assignCourierToDelivery"
+import { assignCourierToDelivery, reassignCourierToDelivery } from "../services/assignCourierToDelivery"
 import { db } from "../../../lib/firebase";
 
 export const useAssignOrdersToDelivery = () => {
@@ -11,20 +11,49 @@ export const useAssignOrdersToDelivery = () => {
     setError(null);
   };
 
-  const assingToDelivery = async (deliveryId: string, orderId: string, courierId: string, reassign: boolean = false): Promise<void> => {
+  const assingToDelivery = async (orderId: string, courierId: string): Promise<void> => {
 
     setIsLoading(true);
     setError(null);
     try {
-      await assignCourierToDelivery(db, deliveryId, orderId, courierId, reassign);
+      await assignCourierToDelivery(db, orderId, courierId);
     } catch {
-      setError(reassign ? 'Error al reasignar el pedido.' : 'Error al asignar el pedido.');
+      setError('Error al asignar el pedido.');
     } finally {
       setIsLoading(false);
     }
   }
   return {
     assingToDelivery,
+    isLoading,
+    error,
+    reset,
+  }
+}
+
+export const useRessignOrdersToDelivery = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const reset = () => {
+    setIsLoading(false);
+    setError(null);
+  };
+
+  const reassingToDelivery = async (deliveryId: string, orderId: string, courierId: string): Promise<void> => {
+
+    setIsLoading(true);
+    setError(null);
+    try {
+      await reassignCourierToDelivery(db, deliveryId, orderId, courierId);
+    } catch {
+      setError('Error al reasignar el pedido.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  return {
+    reassingToDelivery,
     isLoading,
     error,
     reset,
